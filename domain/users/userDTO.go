bfd5a022d504da69609ead5992d0e96f40575e4c
Javad <ja7ad@live.com>
2021-12-09 19:58:10 +0330

    Change to user data transfer object

---
+++ domain/users/userDTO.go
@@ -0,0 +1,26 @@
+// Package users data transfer object
+package users
+
+import (
+	"bookstore/utils/errors"
+	"strings"
+)
+
+// User entity
+type User struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+}
+
+// Validate user entity
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+
+	return nil
+}
