76a57237ed0cb57fd10657d3189591557c39fb50
Javad <ja7ad@live.com>
2021-12-09 19:59:05 +0330

    Add GetUser service and CreateUser service

--- services/users.go
+++ services/users.go
@@ -5,6 +5,24 @@ import (
 	"bookstore/utils/errors"
 )
 
+// GetUser gets a user by its id
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// CreateUser creates a new user
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user, nil
 }
