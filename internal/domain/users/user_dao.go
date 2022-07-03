3a7e189d4fea77a08d345233185b9ddf3ec16d11
idanieldrew <danielgia35@gmail.com>
2022-07-03 15:41:02 +0430

    Change get_user query

--- internal/domain/users/user_dao.go
+++ internal/domain/users/user_dao.go
@@ -13,7 +13,7 @@ import (
 // Mysql queries
 const (
 	queryInsertUser   = "INSERT INTO users(first_name, last_name, email, date_created, status, password) VALUES(?, ?, ?, ?, ?, ?);"
-	queryGetUser      = " id, first_name, last_name, email, date_created, status FROM users WHERE id=?;"
+	queryGetUser      = "SELECT id, first_name, last_name, email, date_created, status FROM users WHERE id=?;"
 	queryUpdateUser   = "UPDATE users SET first_name=?, last_name=?, email=? WHERE id=?;"
 	queryDeleteUser   = "DELETE FROM users WHERE id=?;"
 	queryFindByStatus = "SELECT id, first_name, last_name, email, date_created, status FROM users WHERE status=?;"
